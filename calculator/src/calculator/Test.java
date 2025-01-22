fb2f02b124eae08ce213a288a56cd48c36bf5d83
Saurabh9365 <saurabhborse9365@gmail.com>
2025-01-22 18:55:29 +0530

    add print satement

--- calculator/src/calculator/Test.java
+++ calculator/src/calculator/Test.java
@@ -1,14 +1,16 @@
 package calculator;
 
 public class Test {
+   public static void main(String[] args) {
+	
 
 	int a = 10;
 
-	int b= 2 0;
+	int b= 20;
 
 
   int c = a+b;
 
 	System.out.println(c);
-	
+}
 }
